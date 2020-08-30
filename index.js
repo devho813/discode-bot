5ca613fb2c3a18a5374d10863b11268e212cc60d
devho813 <devho813@gmail.com>
2020-08-31 04:40:12 +0900

    스터디 시작, 종료 기능 구현

--- index.js
+++ index.js
@@ -1,37 +1,110 @@
 const { prefix } = require('./config.json');
 const dotenv = require('dotenv');
-const Discord = require(`discord.js`);
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 dotenv.config();
 
+function parseTime(time) {
+  let hour = Math.floor(((time / (1000 * 60 * 60)) % 24));
+  let minute = Math.floor(((time / (1000 * 60)) % 60));
+  let second = Math.floor((time / 1000) % 60);
+
+  hour = hour < 10 ? '0' + hour : hour;
+  minute = minute < 10 ? '0' + minute : minute;
+  second = second < 10 ? '0' + second : second;
+
+  return `${hour}:${minute}:${second}`
+}
+
+function messageProcess(message, option) {
+  const { author: { id, username } } = message;
+  const user = db.get('users').find({ id }).value();
+  const messageSendTime = new Date().getTime();
+
+  switch (option) {
+    case '시작':
+      if (!user) {
+        // 신규 생성
+        db.get('users').push({
+          id,
+          username,
+          isStudying: true,
+          startTime: messageSendTime,
+          today: 0,
+          week: 0,
+          total: 0,
+        }).write();
+      } else {
+        // 업데이트
+        const { isStudying } = user;
+
+        if (isStudying) {
+          message.channel.send(`👨‍💻  ${username}님은 이미 학습 중입니다.`)
+          return;
+        }
+
+        db.get('users')
+          .find({ id })
+          .assign({
+            isStudying: true,
+            startTime: messageSendTime,
+          })
+          .write();
+      }
+
+      message.channel.send(`
+          🚀  ${username}님이 스터디를 시작하셨습니다. \`\`\`누적 시간은 스터디 종료 시 확인 가능합니다.\`\`\`
+      `);
+      break;
+    case '종료':
+      const { isStudying, startTime, today, week, total } = user;
+
+      if (!user || !isStudying) {
+        message.channel.send(`⚠  스터디를 시작하지 않았습니다.`);
+        return;
+      }
+
+      const studyAmount = messageSendTime - startTime;
+
+      db.get('users')
+        .find({ id })
+        .assign({
+          isStudying: false,
+          startTime: 0,
+          today: today + studyAmount,
+          week: week + studyAmount,
+          total: total + studyAmount
+        })
+        .write();
+
+      message.channel.send(`
+          🎉  ${username}님이 스터디를 종료하셨습니다. \`\`\`금일 누적: ${parseTime(today + studyAmount)}\n금주 누적: ${parseTime(week + studyAmount)}\n총 누적: ${parseTime(total + studyAmount)}\`\`\`
+      `);
+      break;
+    default:
+      message.channel.send(`⚠ 잘못된 명령어입니다!`);
+  }
+}
+
 client.once('ready', () => {
+  db.defaults({ users: [] }).write();
   console.log("스터디 봇이 준비되었습니다");
 });
 
 client.on('message', message => {
-  if (message.channel.type == 'dm') return
-  if (!message.content.startsWith(prefix)) return
+  const [command, option] = message.content.split(" ");
 
-  const mes = message.content.split(" ");
-  if (mes[0] === `${prefix}스터디`) {
-    switch (mes[1]) {
-      case '시작':
-        message.channel.send(`
-            🔥  ${message.author.username}님이 스터디를 시작하셨습니다.
-            \`\`\`금일 누적: 03:32:02 (5위)\n금주 누적: 08:12:34 (5위)\n총 누적: 13:22:34 (5위)\`\`\`
-        `);
-        break;
-      case '종료':
-        message.channel.send(`
-            🎉  ${message.author.username}님이 스터디를 종료하셨습니다.
-          \`\`\`금일 누적: 03:32:02 (5위)\n금주 누적: 08:12:34 (5위)\n총 누적: 13:22:34 (5위)\`\`\`
-        `);
-        break;
-      default:
-        message.channel.send(`⚠ 잘못된 명령어입니다!`);
-    }
-  }
+  if (message.channel.type == 'dm') return;
+  if (!message.content.startsWith(prefix)) return;
+  if (command !== `${prefix}스터디`) return;
+
+  messageProcess(message, option);
 });
 
 client.login(process.env.TOKEN);
